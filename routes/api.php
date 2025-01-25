33a976f5fa8018bb158b6be623d556b389cdd6a5
Brightsunny <106729314+B619Ht@users.noreply.github.com>
2025-01-25 15:59:56 +0530

    (fix)modification

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\CommentController;
 
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('posts', PostController::class);
-    Route::apiResource('posts.comments', CommentController::class);
-});
+
+Route::apiResource('posts', PostController::class);
+Route::apiResource('posts.comments', CommentController::class);
